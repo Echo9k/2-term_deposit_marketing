3d41c9311afed6254ff3b7071bd32d2826505880
Guillermo Alcántara <32722692+Echo9k@users.noreply.github.com>
2024-10-13 18:02:37 +0000

    updated helping functions

--- term_deposit/util.py
+++
@@ -1,7 +0,0 @@
-from sklearn.metrics import recall_score
-
-# Define a custom recall function for class 1 with logging
-def recall_class_1_function(y_true, y_pred, **kwargs):
-    # Print or log y_pred to check if class 1 is being predicted
-    print("y_pred distribution:", pd.Series(y_pred).value_counts())  # Logging prediction distribution
-    return recall_score(y_true, y_pred, pos_label=1, **kwargs)
